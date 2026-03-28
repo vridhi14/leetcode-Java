2a4179cfb04871d8e1748a22a8592e54bc76b218
Vridhi <rajeevvridhi@gmail.com>
2026-03-28 18:27:52 +0530

    Time: 4 ms (76.38%), Space: 47.8 MB (92.75%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
@@ -9,22 +7,21 @@ class Solution {
             freq[t - 'A']++;
         }
 
+        // Sort frequencies
         Arrays.sort(freq);
 
         int maxFreq = freq[25];
         int countMax = 1;
 
-        // Count how many have same max frequency
+        // Count how many have max frequency
         for (int i = 24; i >= 0; i--) {
             if (freq[i] == maxFreq) countMax++;
             else break;
         }
 
-        int partCount = maxFreq - 1;
-        int partLength = n + 1;
-
-        int result = partCount * partLength + countMax;
-
-        return Math.max(tasks.length, result);
+        return Math.max(
+            tasks.length,
+            (maxFreq - 1) * (n + 1) + countMax
+        );
     }
 }
