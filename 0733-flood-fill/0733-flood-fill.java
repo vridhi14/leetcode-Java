82dd0c2f44cd64af46e78a7e2d550958f9b16afa
Vridhi <rajeevvridhi@gmail.com>
2026-05-24 20:11:53 +0530

    Time: 0 ms (100%), Space: 46.8 MB (59.82%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int originalColor = image[sr][sc];
-        
-        if (originalColor == newColor) return image;
-        
-        dfs(image, sr, sc, originalColor, newColor);
-        return image;
+       int original = image[sr][sc];
+       if(original == newColor){return image ; }
+       dfs(image , sr , sc , original , newColor);
+       return image ; 
     }
-    
-    private void dfs(int[][] image, int r, int c, int originalColor, int newColor) {
-        int n = image.length;
-        int m = image[0].length;
+    public void dfs(int[][]image , int r, int c , int original , int color){
+        if(r<0 || c<0 || r>=image.length || c>=image[0].length)return ; 
+
+        if(image[r][c]!= original)return ; 
+
+        image[r][c]= color ; 
         
-        // Boundary + color check
-        if (r < 0 || c < 0 || r >= n || c >= m || image[r][c] != originalColor) {
-            return;
-        }
-        
-        // Change color
-        image[r][c] = newColor;
-        
-        // Explore 4 directions
-        dfs(image, r + 1, c, originalColor, newColor); // down
-        dfs(image, r - 1, c, originalColor, newColor); // up
-        dfs(image, r, c + 1, originalColor, newColor); // right
-        dfs(image, r, c - 1, originalColor, newColor); // left
-    }
+        dfs(image , r+1 , c , original , color);
+        dfs(image , r-1 , c , original , color);
+        dfs(image , r , c+1 , original , color);
+        dfs(image , r , c-1 , original , color);
+    } 
 }
