8d03e56fe1a26604310a6b2244948c887bec3caa
Vridhi <rajeevvridhi@gmail.com>
2026-05-25 19:48:18 +0530

    Time: 6 ms (98.89%), Space: 60.8 MB (45.42%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,84 +1,46 @@
 class Solution {
-
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
-
     public int numEnclaves(int[][] grid) {
-
-        int m = grid.length;
-        int n = grid[0].length;
-
-        // STEP 1:
-        // Traverse boundary cells
-
-        for (int i = 0; i < m; i++) {
-
-            // first column
-            if (grid[i][0] == 1) {
-                dfs(grid, i, 0);
+        int m = grid.length ; 
+        int n = grid[0].length ;
+        int count = 0 ;  
+        for(int i = 0;i<m;i++){
+            if(grid[i][0]==1){
+                dfs(grid , i , 0);
             }
-
-            // last column
-            if (grid[i][n - 1] == 1) {
-                dfs(grid, i, n - 1);
+            if(grid[i][n-1]==1){
+                dfs(grid , i , n-1);
+            }
+        }
+        for(int j = 0; j<n; j++){
+            if(grid[0][j]==1){
+                dfs(grid , 0 , j);
+            }
+            if(grid[m-1][j]==1){
+                dfs(grid , m-1, j);
             }
         }
 
-        for (int j = 0; j < n; j++) {
-
-            // first row
-            if (grid[0][j] == 1) {
-                dfs(grid, 0, j);
-            }
-
-            // last row
-            if (grid[m - 1][j] == 1) {
-                dfs(grid, m - 1, j);
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+               if(grid[i][j] == 1){
+                count++;
+                } 
             }
         }
-
-        // STEP 2:
-        // Count remaining land cells
-
-        int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (grid[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+        return count ; 
     }
 
-    private void dfs(int[][] grid, int r, int c) {
+    public void dfs(int[][]grid , int row , int col){
+        int m= grid.length;
+        int n= grid[0].length ; 
 
-        int m = grid.length;
-        int n = grid[0].length;
-
-        // out of bounds
-        if (r < 0 || c < 0 || r >= m || c >= n) {
-            return;
+        if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] != 1){
+            return ; 
         }
-
-        // water or already visited
-        if (grid[r][c] == 0) {
-            return;
-        }
-
-        // mark visited
-        grid[r][c] = 0;
-
-        // explore 4 directions
-        for (int k = 0; k < 4; k++) {
-
-            int nr = r + dr[k];
-            int nc = c + dc[k];
-
-            dfs(grid, nr, nc);
-        }
+        grid[row][col] = 0 ; 
+        dfs(grid , row + 1 , col);
+        dfs(grid , row - 1 , col);
+        dfs( grid , row , col + 1);
+        dfs( grid , row , col - 1);
     }
 }
