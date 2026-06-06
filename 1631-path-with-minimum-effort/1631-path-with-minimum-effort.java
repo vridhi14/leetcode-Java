6d2aa7e9b8d785a145459b13bd672e7f7c229c23
Vridhi <rajeevvridhi@gmail.com>
2026-06-06 19:54:23 +0530

    Time: 39 ms (85.51%), Space: 47.9 MB (56.32%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,79 +1,53 @@
-import java.util.*;
-
-class Solution {
-
-    class Pair {
-        int effort;
-        int row;
-        int col;
-
-        Pair(int effort, int row, int col) {
-            this.effort = effort;
-            this.row = row;
-            this.col = col;
+class Solution{
+    public int minimumEffortPath(int[][] heights){
+        int m = heights.length ; 
+        int n = heights[0].length ; 
+        int[][] effort = new int[m][n]; 
+        for(int i = 0 ; i< m ; i++){
+            Arrays.fill(effort[i] , Integer.MAX_VALUE);
         }
-    }
-
-    public int minimumEffortPath(int[][] heights) {
-
-        int m = heights.length;
-        int n = heights[0].length;
-
-        int[][] effort = new int[m][n];
-
-        for (int[] row : effort) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
+        PriorityQueue<int[]> pq =  new PriorityQueue<>((a, b) -> a[0] - b[0]);
+         pq.offer(new int[]{0, 0, 0});
 
         effort[0][0] = 0;
 
-        PriorityQueue<Pair> pq =
-                new PriorityQueue<>((a, b) -> a.effort - b.effort);
-
-        pq.offer(new Pair(0, 0, 0));
-
-        int[][] directions = {
-                {1, 0},
-                {-1, 0},
-                {0, 1},
-                {0, -1}
+        int[][] dir = {
+                {1,0},
+                {-1,0},
+                {0,1},
+                {0,-1}
         };
 
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll(); 
 
-            Pair curr = pq.poll();
+            int currEffort = curr[0]; 
+            int row = curr[1]; 
+            int col = curr[2];
 
-            int currEffort = curr.effort;
-            int r = curr.row;
-            int c = curr.col;
+            if(row == m-1 && col == n-1) return currEffort ; 
 
-            if (r == m - 1 && c == n - 1) {
-                return currEffort;
-            }
+            for(int[] d : dir){
+                int nr = row + d[0]; 
+                int nc = col + d[1]; 
 
-            for (int[] dir : directions) {
-
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-
-                    int edgeCost =
-                            Math.abs(heights[r][c] - heights[nr][nc]);
-
-                    int newEffort =
-                            Math.max(currEffort, edgeCost);
-
-                    if (newEffort < effort[nr][nc]) {
+                if(nr>=0 && nc>=0 && nr<m && nc<n){
+                    int diff = Math.abs(heights[row][col] - heights[nr][nc]); 
+                      int newEffort =
+                            Math.max(currEffort, diff);
+                               if (newEffort < effort[nr][nc]) {
 
                         effort[nr][nc] = newEffort;
 
-                        pq.offer(new Pair(newEffort, nr, nc));
+                        pq.offer(new int[]{
+                                newEffort,
+                                nr,
+                                nc
+                        });
                     }
                 }
+                }
             }
+            return 0 ; 
         }
-
-        return 0;
     }
-}
