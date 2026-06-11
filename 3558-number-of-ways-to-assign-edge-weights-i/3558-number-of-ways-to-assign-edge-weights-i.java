73b4d093e869c2edad7ce15685e48f9782ff879e
Vridhi <rajeevvridhi@gmail.com>
2026-06-11 21:13:04 +0530

    Time: 84 ms (78.57%), Space: 279.5 MB (61.9%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+
+    private static final long MOD = 1_000_000_007L;
+
+    public int assignEdgeWeights(int[][] edges) {
+
+        int n = edges.length + 1;
+
+        List<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] vis = new boolean[n + 1];
+
+        q.offer(1);
+        vis[1] = true;
+
+        int depth = -1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            depth++;
+
+            while (size-- > 0) {
+                int node = q.poll();
+
+                for (int nei : graph[node]) {
+                    if (!vis[nei]) {
+                        vis[nei] = true;
+                        q.offer(nei);
+                    }
+                }
+            }
+        }
+
+        return (int) modPow(2, depth - 1);
+    }
+
+    private long modPow(long base, long exp) {
+        long ans = 1;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                ans = (ans * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        return ans;
+    }
+}
