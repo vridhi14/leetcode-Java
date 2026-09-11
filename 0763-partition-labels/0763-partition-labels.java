31bb8613ef4f6b61e6112139abe9692323e11bca
Vridhi <rajeevvridhi@gmail.com>
2026-09-11 18:10:40 +0530

    Time: 5 ms (81%), Space: 43.2 MB (89.48%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26]; 
+
+        for(int i = 0 ; i<s.length() ;  i++){
+            last[s.charAt(i) - 'a'] = i; 
+        }
+        List<Integer> result = new ArrayList<>();
+
+        int start = 0 ; 
+        int end = 0 ; 
+
+        for(int i = 0 ; i<s.length(); i++){
+            end = Math.max(end, last[s.charAt(i) - 'a']); 
+
+            if(i == end){
+                result.add(end - start + 1); 
+                start = i + 1;
+            }
+        }
+        return result ; 
+    }
+}
